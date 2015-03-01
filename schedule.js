abc4d881e537c1d223164064934dc0ddc87ceb4a
Tatsumi Suenaga <suenaga.1@osu.edu>
2015-03-01 16:09:35 -0500

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -2,7 +2,9 @@
 var today = new Date();
 function change(){
 	var button = document.getElementById("fold_button");
+	var button2 = document.getElementById("jump_button");
 	if (button.value=="Fold") {
+		button2.style.visibility = "collapse";
 		button.value = "Unfold?";
 		var row = document.getElementsByClassName("show");
 		for (var i = 0; i < row.length; ++i) {
@@ -16,21 +18,35 @@ function change(){
 		}
 	} else {
 		button.value = "Fold";
+		button2.style.visibility = "visible";
 		var row = document.getElementsByTagName("tr");
 		for (var i = 0; i < row.length; i++) {
 			row[i].style.visibility = "visible";
 		}
 	}
 }
+today.setMonth(3);
+today.setDate(4);
 function jumpScroll() {
-	window.scrollBy(0,125);
-	if (today.getDate == 1){
-		window.scrollBy(0,880);
-	} else if (today.getDate > 1) {
-		var s = 880;
-		for (var i = 0; i < (today.getDate - 1); i++) {
- 			s += 30;
+	//only need 45 * 30px to reach May 1st
+	var button = document.getElementById("fold_button");
+	if (button.value = "Fold"){
+		var scroll = 155;
+		var row = document.getElementsByClassName("show");
+		for(var i = 0; i < 45; i++) {
+			if (parseInt(row[i].getElementsByTagName("td")[2].className) < (today.getMonth()+1)){
+					scroll += 36;
+			} else if (parseInt(row[i].getElementsByTagName("td")[2].className) == today.getMonth()){
+				if(parseInt(row[i].getElementsByTagName("td")[2].id) < today.getDate()){
+					scroll += 36;
+				} else{
+					i = 45;
+				}
 		}
-		window.scrollBy(0,s);
+			
+		}
+		window.scrollTo(0,scroll);
+	} else {
+		window.alert("No need to jump since table was folded!");
 	}
 }
